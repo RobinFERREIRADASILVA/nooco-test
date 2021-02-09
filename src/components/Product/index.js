4246fc34b210fb3fa0316770c0a22f2aaf6223f5
Robin FERREIRA DA SILVA <ferreiradasilvarobin@gmail.com>
2021-02-09 10:22:26 +0100

    get the good datas for new graph

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,14 +1,29 @@
+/* eslint-disable no-plusplus */
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
 // Import data
-import { products } from 'src/data';
+import { products, logClics } from 'src/data';
 
 import './product.scss';
 
 const Product = () => {
-  const brand = products.map((product) => product.brand);
-  console.log(brand);
+  // Get the brand's name with article
+  const brandWithArticle = products.map((product) => `${product.brand}-${product.article}`);
+
+  // Get the number of sales for each article
+  const numberOfSales = products.map((product) => {
+    let count = 0;
+    const loopOnLog = logClics.map((log) => {
+      if (product.id === log.idProduct) {
+        if (log.purchase === true) {
+          count++;
+        }
+      }
+    });
+    return count;
+  });
+
   return (
     <div className="product">
       <h2>Statistiques des produits</h2>
@@ -16,15 +31,19 @@ const Product = () => {
         <div className="product-first-graph">
           <Bar
             data={{
-              labels: ['Gap', 'Levis', 'Diesel'],
+              labels: brandWithArticle,
               datasets: [
                 {
-                  label: 'Nombres de connexions par mois',
-                  data: [0,10,20,30],
+                  label: 'Nombre de ventes',
+                  data: numberOfSales,
                   backgroundColor: [
                     'rgba(63, 0, 208, 0.5)',
                     'rgba(158, 198, 190, 0.5)',
                     'rgba(55, 64, 100, 0.5)',
+                    'rgba(145, 64, 100, 0.5)',
+                    'rgba(55, 75, 100, 0.5)',
+                    'rgba(55, 25, 100, 0.5)',
+                    'rgba(55, 158, 100, 0.5)',
                   ],
                 },
               ],
